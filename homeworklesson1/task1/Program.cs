fa15f84e85fa41e7b232c61948784984db43fbb5
lain <fgjhfgjf@gmail.com>
2023-09-27 21:55:37 +0300

    выполнил первое задание

---
+++ homeworklesson1/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int fnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int snumber = Convert.ToInt32(Console.ReadLine());
+
+if (fnumber>snumber)
+{
+    Console.WriteLine($"max-> {fnumber}");
+}
+else
+{
+    Console.WriteLine($"max-> {snumber}");
+}
+
